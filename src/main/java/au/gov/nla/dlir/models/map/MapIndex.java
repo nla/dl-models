192ea24844afa53b94e7dc40c569c680ab7a94de
bsingh-nla <bsingh@nla.gov.au>
2015-11-19 12:59:00 +1100

    map related metadat moved to pojo

---
+++ src/main/java/au/gov/nla/dlir/models/map/MapIndex.java
@@ -0,0 +1,79 @@
+package au.gov.nla.dlir.models.map;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class MapIndex {
+    private static final String MAP_COPY_ROLE_INDEX = "i";
+    private static final String MAP_COPY_ROLE_FLIGHT_DIAGRAM = "fd";
+    
+    private String pid;
+    private List<String> copyRole;
+    private boolean hasIndexCopy;
+    private boolean hasFlightDiagramCopy;
+    private String interactiveHtml;
+    private List<Neighbour> navigation;
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public List<String> getCopyRole() {
+        return copyRole;
+    }
+
+    public void setCopyRole(List<String> copyRole) {
+        this.copyRole = copyRole;
+        populateCopyRoleFlags(copyRole);
+    }
+
+    private void populateCopyRoleFlags(List<String> copyRole) {
+        if (copyRole != null && !copyRole.isEmpty()) {
+            if (copyRole.contains(MAP_COPY_ROLE_INDEX)) {
+                setHasIndexCopy(true);
+            }
+
+            if (copyRole.contains(MAP_COPY_ROLE_FLIGHT_DIAGRAM)) {
+                setHasFlightDiagramCopy(true);
+            }
+        }
+    }
+
+    public boolean isHasIndexCopy() {
+        return hasIndexCopy;
+    }
+
+    public void setHasIndexCopy(boolean hasIndexCopy) {
+        this.hasIndexCopy = hasIndexCopy;
+    }
+
+    public boolean isHasFlightDiagramCopy() {
+        return hasFlightDiagramCopy;
+    }
+
+    public void setHasFlightDiagramCopy(boolean hasCopyRoleFlightDiagram) {
+        this.hasFlightDiagramCopy = hasCopyRoleFlightDiagram;
+    }
+
+    public String getInteractiveHtml() {
+        return interactiveHtml;
+    }
+
+    public void setInteractiveHtml(String interactiveHtml) {
+        this.interactiveHtml = interactiveHtml;
+    }
+
+    public List<Neighbour> getNavigation() {
+        return navigation;
+    }
+
+    public void setNavigation(List<Neighbour> navigation) {
+        this.navigation = navigation;
+    }
+}
