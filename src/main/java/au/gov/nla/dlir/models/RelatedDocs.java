16b15597c4a8a58410b886a5ad1675f76de3551a
amuller <amuller@nla.gov.au>
2016-01-29 16:07:53 +1100

    Revert needed files.

---
+++ src/main/java/au/gov/nla/dlir/models/RelatedDocs.java
@@ -0,0 +1,18 @@
+package au.gov.nla.dlir.models;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class RelatedDocs {
+    List<RelatedDocument> docs;
+
+    public List<RelatedDocument> getDocs() {
+        return docs;
+    }
+
+    public void setDocs(List<RelatedDocument> documents) {
+        this.docs = documents;
+    }
+}
