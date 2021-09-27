6f7895817802627224999f6085cb7fdde3ae3d9b
JMitchell-NLA <jmitchell@nla.gov.au>
2021-09-27 23:56:21 +1000

    Revises wording for Copyright responses as per Bronwyns instructions

--- src/main/java/au/gov/nla/dlir/models/CopyrightStatusResponse.java
+++ src/main/java/au/gov/nla/dlir/models/CopyrightStatusResponse.java
@@ -34,16 +34,16 @@ public class CopyrightStatusResponse {
                 message = "You may copy under some circumstances, for example you may copy a portion for research or study. Order a copy. Contact us for further information about copying.";
                 break;
             case "1.2":
-                message = "The catalogue record indicates that this work is out of copyright. If the work contains any underlying material, such as a script, music or illustration, these may remain in copyright. Contact us for further information about copying.";
+                message = "You may order a copy through Copies Direct or use the online copy for research or study; for other uses contact us.";
                 break; 
             case "2.1":
-                message = "The catalogue record indicates that this work is out of copyright. If the work contains any underlying material, such as a script, music or illustration, these may remain in copyright. Contact us for further information about copying.";
+                message = "You may order a copy through Copies Direct or use the online copy for research or study; for other uses contact us.";
                 break;
             case "1.3": 
                 message = "You may have full rights to copy, or may be able to copy only under some circumstances, for example a portion for research or study. Contact us for further information about copying."; 
                 break;
             case "2.2":
-                message = "Copyright varies with each issue and article. You may have full rights to copy, or may be able to copy only under some circumstances, for example a portion for research or study. Contact us for further information about copying. You may copy or order a copy.";
+                message = "Copyright varies with each issue and article. You may have full rights to copy, or may be able to copy only under some circumstances, for example a portion for research or study. Order a copy where circumstances allow or contact us for further information.";
                 break;
             case "3": 
                 break;
