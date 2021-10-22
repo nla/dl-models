5e9a9704d6a2b85958e5654243a381221afbd786
aburra <aburra@nla.gov.au>
2021-10-22 12:17:55 +1100

    refactor: text correction message model (MDCU-708)

--- src/main/java/au/gov/nla/dlir/models/event/TextCorrectionMessage.java
+++ src/main/java/au/gov/nla/dlir/models/event/TextCorrectionMessage.java
@@ -9,8 +9,11 @@ import lombok.ToString;
 @Setter
 @ToString(callSuper = true)
 public class TextCorrectionMessage extends Payload {
+  public enum ArticleType {NEWSPAPER, GAZETTE, DLC;}
+
   private String user;
   private String objId;
   private String copyId;
+  private ArticleType articleType;
   private List<Correction> corrections;
 }
