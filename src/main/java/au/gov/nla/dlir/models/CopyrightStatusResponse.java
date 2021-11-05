2877f6a4848aae8cb882fc21845367237106b108
JMitchell-NLA <jmitchell@nla.gov.au>
2021-11-05 16:47:23 +1100

    In CopyrightStatusResponse, change all instances of 'contact us' to 'Contact us' to make the link substitute in tarkine

--- src/main/java/au/gov/nla/dlir/models/CopyrightStatusResponse.java
+++ src/main/java/au/gov/nla/dlir/models/CopyrightStatusResponse.java
@@ -34,16 +34,16 @@ public class CopyrightStatusResponse {
                 message = "You may copy under some circumstances, for example you may copy a portion for research or study. Order a copy. Contact us for further information about copying.";
                 break;
             case "1.2":
-                message = "You may order a copy through Copies Direct or use the online copy for research or study; for other uses contact us.";
+                message = "You may order a copy through Copies Direct or use the online copy for research or study; for other uses Contact us.";
                 break; 
             case "2.1":
-                message = "You may order a copy through Copies Direct or use the online copy for research or study; for other uses contact us.";
+                message = "You may order a copy through Copies Direct or use the online copy for research or study; for other uses Contact us.";
                 break;
             case "1.3": 
                 message = "You may have full rights to copy, or may be able to copy only under some circumstances, for example a portion for research or study. Contact us for further information about copying."; 
                 break;
             case "2.2":
-                message = "Copyright varies with each issue and article. You may have full rights to copy, or may be able to copy only under some circumstances, for example a portion for research or study. Order a copy where circumstances allow or contact us for further information.";
+                message = "Copyright varies with each issue and article. You may have full rights to copy, or may be able to copy only under some circumstances, for example a portion for research or study. Order a copy where circumstances allow or Contact us for further information.";
                 break;
             case "3": 
                 break;
